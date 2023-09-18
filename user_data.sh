6dbd1a501ab50af4c6f4506b77a6450ec25c3b58
mbreddy507 <ubuntu@ip-172-31-44-12.ec2.internal>
2023-09-18 07:26:18 +0000

    added ecs_cluster,bastion_host,sg

---
+++ user_data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+## Configure cluster name using the template variable ${ecs_cluster_name}
+
+echo ECS_CLUSTER='${ecs_cluster_name}' >> /etc/ecs/ecs.config
+
--- vars.tf
+++ vars.tf
@@ -25,3 +25,15 @@ variable "az_count" {
   default     = 2
   type        = number
 }
+
+variable "instance_type" {
+  description = "Instance type for EC2"
+  default     = "t3.micro"
+  type        = string
+}
+
+variable "public_ec2_key" {
+  description = "Public key for SSH access to EC2 instances"
+  default	= "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC8XRdUYQ0/tSIJHW2wisvqTuWK4PnCDKVOkxrncxSAPlmsNREH4IUOKnXOUykUpk4o/C2wUDdCR2V/xrzXWLllxCRLyS9z2/+W4BN6QQodcYOckyi7H7/HlU+JybyhB28cGEvE/2FvFen/lOhf8N+NJ4KO0pKJLy778LdBCUfzPtmEcAAtoS2w13Rogj3rciCtq5TTmdUgDj3yDhUGgE+CTmXZxOqGekGtSlxDQBp7SfgWYpBsEe1R3Wpx6LGgFwcsFMgx5p4xPrufMBuPvrvKjHA44FY9dhmuw5h2CQi/DPyNs64bkt8n1WC+s/Q9U/ohPd04lsV1xJNX/zWAl4fYWpH/x9l7rNmQpG6NCMDVC2foMHHmm7NhmmG/r/omOA0/+wd6Ao9o7JrQqY8cFF+bA2xIr7Gp6QQwxH2klELCc8xVdArJQZ1+m6y3B045CLVK2nKcbQllObkJsT1TsAzrWzt94ITuypZ1wU7s7xwMJ9nmE8MjdlskxNg3OGB0nDk= ubuntu@ip-172-31-44-12"  
+  type        = string
+}
